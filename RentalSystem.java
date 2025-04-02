0621ebf996eb680ac8bf07b9f0468db369a7ec3d
ChillEzi <ezinwanwakoby@trentu.ca>
2025-04-02 13:56:59 -0400

    Refactor RentalSystem for Singleton Pattern Implementation

--- RentalSystem.java
+++ RentalSystem.java
@@ -3,9 +3,20 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class RentalSystem {
+    private static RentalSystem instance; 
+
     private List<Vehicle> vehicles = new ArrayList<>();
     private List<Customer> customers = new ArrayList<>();
     private RentalHistory rentalHistory = new RentalHistory();
+
+    private RentalSystem() {}
+
+    public static RentalSystem getInstance() {
+        if (instance == null) {
+            instance = new RentalSystem();
+        }
+        return instance;
+    }
 
     public void addVehicle(Vehicle vehicle) {
         vehicles.add(vehicle);
@@ -20,8 +31,7 @@ public class RentalSystem {
             vehicle.setStatus(Vehicle.VehicleStatus.RENTED);
             rentalHistory.addRecord(new RentalRecord(vehicle, customer, date, amount, "RENT"));
             System.out.println("Vehicle rented to " + customer.getCustomerName());
-        }
-        else {
+        } else {
             System.out.println("Vehicle is not available for renting.");
         }
     }
@@ -31,16 +41,15 @@ public class RentalSystem {
             vehicle.setStatus(Vehicle.VehicleStatus.AVAILABLE);
             rentalHistory.addRecord(new RentalRecord(vehicle, customer, date, extraFees, "RETURN"));
             System.out.println("Vehicle returned by " + customer.getCustomerName());
-        }
-        else {
+        } else {
             System.out.println("Vehicle is not rented.");
         }
-    }    
+    }
 
     public void displayAvailableVehicles() {
-    	System.out.println("|     Type         |\tPlate\t|\tMake\t|\tModel\t|\tYear\t|");
-    	System.out.println("---------------------------------------------------------------------------------");
-    	 
+        System.out.println("|     Type         |\tPlate\t|\tMake\t|\tModel\t|\tYear\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+
         for (Vehicle v : vehicles) {
             if (v.getStatus() == Vehicle.VehicleStatus.AVAILABLE) {
                 System.out.println("|     " + (v instanceof Car ? "Car          " : "Motorcycle   ") + "|\t" + v.getLicensePlate() + "\t|\t" + v.getMake() + "\t|\t" + v.getModel() + "\t|\t" + v.getYear() + "\t|\t");
@@ -48,7 +57,7 @@ public class RentalSystem {
         }
         System.out.println();
     }
-    
+
     public void displayAllVehicles() {
         for (Vehicle v : vehicles) {
             System.out.println("  " + v.getInfo());
@@ -60,13 +69,13 @@ public class RentalSystem {
             System.out.println("  " + c.toString());
         }
     }
-    
+
     public void displayRentalHistory() {
         for (RentalRecord record : rentalHistory.getRentalHistory()) {
             System.out.println(record.toString());
         }
     }
-    
+
     public Vehicle findVehicleByPlate(String plate) {
         for (Vehicle v : vehicles) {
             if (v.getLicensePlate().equalsIgnoreCase(plate)) {
@@ -75,18 +84,22 @@ public class RentalSystem {
         }
         return null;
     }
-    
+
     public Customer findCustomerById(int id) {
-        for (Customer c : customers)
-            if (c.getCustomerId() == id)
+        for (Customer c : customers) {
+            if (c.getCustomerId() == id) {
                 return c;
+            }
+        }
         return null;
     }
 
     public Customer findCustomerByName(String name) {
-        for (Customer c : customers)
-            if (c.getCustomerName().equalsIgnoreCase(name))
+        for (Customer c : customers) {
+            if (c.getCustomerName().equalsIgnoreCase(name)) {
                 return c;
+            }
+        }
         return null;
     }
 }
