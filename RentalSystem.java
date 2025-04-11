07a78a0eca36deebc51a8b2b11a30fc154919993
ChillEzi <ezinwanwakoby@trentu.ca>
2025-04-11 17:35:23 -0400

    Prevent addition of vehicles with same license plate and customers with same ID; update return types to boolean

--- RentalSystem.java
+++ RentalSystem.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -23,15 +22,25 @@ public class RentalSystem {
         }
         return instance;
     }
-
-    public void addVehicle(Vehicle vehicle) {
+    
+    public boolean addVehicle(Vehicle vehicle) {
+        if (findVehicleByPlate(vehicle.getLicensePlate()) != null) {
+            System.out.println("Error: A vehicle with the license plate '" + vehicle.getLicensePlate() + "' already exists.");
+            return false;
+        }
         vehicles.add(vehicle);
         saveVehicle(vehicle);
+        return true;
     }
 
-    public void addCustomer(Customer customer) {
+    public boolean addCustomer(Customer customer) {
+        if (findCustomerById(customer.getCustomerId()) != null) {
+            System.out.println("Error: A customer with ID '" + customer.getCustomerId() + "' already exists.");
+            return false;
+        }
         customers.add(customer);
         saveCustomer(customer);
+        return true;
     }
 
     public void rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
